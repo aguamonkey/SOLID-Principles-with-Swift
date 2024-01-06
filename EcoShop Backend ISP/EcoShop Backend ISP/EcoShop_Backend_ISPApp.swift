690c14e78981d9cd20657f3a73b261ec0976e094
aguamonkey <joshuabrowne1@icloud.com>
2024-01-06 13:04:08 +0000

    Updating mac

---
+++ EcoShop Backend ISP/EcoShop Backend ISP/EcoShop_Backend_ISPApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoShop_Backend_ISPApp.swift
+//  EcoShop Backend ISP
+//
+//  Created by Gobias LTD on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct EcoShop_Backend_ISPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
